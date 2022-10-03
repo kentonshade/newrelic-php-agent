72f8722134e81277994ba7794cd9d0009f82763f
Kenton Shade, Lead Workflow Engineer <kentonshade@gmail.com>
2022-10-03 16:46:11 -0400

    Missed user-data updates, limiting input number
    [Ticket: None]

--- ec2/run-instances/user_data.sh
+++ ec2/run-instances/user_data.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# Secrets
+
 # Need to determine the best way to manage users on the instance
 sleep 5
 
-# Approach 2:
-# Check for  non-interactive or other options for run.sh
-# The -i flag is critical, this command should now work.
-sudo -i -u ec2-user -H -b sh -c "cd /home/ec2-user/actions-runner; ./run.sh" 2>&1 | tee call-runner-output.txt
 
-# We need to run command as hudson
-# sudo -H -u hudson bash -c 'echo "I am $USER, with uid $UID"'
-# sudo -H -u ec2-user bash -c '/home/ec2-user/actions-runner/run.sh'
+sudo -i -u ec2-user -H -b bash << EOF
+sudo yum install -y jq
+
+# We need to add these to .bashrc and then source
+echo "export GH_TOKEN=${GH_TOKEN}" >> ~/.bashrc
+echo "export RUNNER_NAME=${RUNNER_NAME}" >> ~/.bashrc
+echo "export OWNER=${OWNER}" >> ~/.bashrc
+echo "export REPO=${REPO}" >> ~/.bashrc
+source ~/.bashrc
+EOF
+
+# This must be created on the AMI
+sudo -i -u ec2-user "/home/ec2-user/runner-test.sh"
 sleep 10
-# cd
-# ./actions-runner/run.sh
